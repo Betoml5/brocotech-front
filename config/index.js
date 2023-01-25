15f074e28bc3e41519c5cfa58a341adc0ff23984
Betoml5 <betoml5@hotmail.com>
2023-01-24 20:57:52 -0600

    Fix config

--- config/index.js
+++ config/index.js
@@ -1,9 +1,9 @@
 export const server =
-  process.env.NEXT_PUBLIC_NODE_ENV === "production"
+  process.env.NODE_ENV === "production"
     ? process.env.NEXT_PUBLIC_API_URL
     : "http://localhost:1337/api";
 
-export const isDev = process.env.NEXT_PUBLIC_NODE_ENV === "development";
+export const isDev = process.env.NODE_ENV === "development";
 export const imagePrefix = isDev
   ? "http://localhost:1337"
   : process.env.NEXT_PUBLIC_API_BASE_URL;
