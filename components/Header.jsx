4647c8ace0d06aa63a6707d4b6c8e117e8530f68
Betoml5 <betoml5@hotmail.com>
2023-01-25 19:07:47 -0600

    Fix logo styles

--- components/Header.jsx
+++ components/Header.jsx
@@ -8,16 +8,15 @@ const Header = () => {
   return (
     <header className="flex items-center justify-between shadow-lg p-4">
       <Link className="flex items-center " href="/">
-        <div className="bg-black p-1 rounded-full">
-          <Image
-            className="w-14  "
-            src="/brocologo.png"
-            width={1280}
-            height={1024}
-            alt="brocotech"
-          />
-        </div>
-        <h1 className="ml-4 font-Montserrat text-xl">Broco Tech</h1>
+        <Image
+          className="w-16 h-16  "
+          src="/brocologo.png"
+          width={1280}
+          height={1024}
+          alt="brocotech"
+        />
+
+        <h1 className="ml-2 font-Montserrat text-xl">Broco Tech</h1>
       </Link>
       <div className="flex flex-row-reverse gap-x-4">
         <Link
